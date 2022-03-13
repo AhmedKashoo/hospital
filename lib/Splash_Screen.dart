8d5f90add74396e7bce1c80473e7f1f3cfe0c4b2
hodpital <ahmed.kashoo99@gmail.com>
2022-03-13 12:38:44 +0200

    kaskoo

--- lib/Splash_Screen.dart
+++ lib/Splash_Screen.dart
@@ -1,5 +1,6 @@
 import 'package:easy_splash_screen/easy_splash_screen.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Splash_Screen extends StatefulWidget {
   const Splash_Screen({Key? key}) : super(key: key);
@@ -11,12 +12,13 @@ class Splash_Screen extends StatefulWidget {
 class _Splash_ScreenState extends State<Splash_Screen> {
   @override
   Widget build(BuildContext context) {
-    return  EasySplashScreen(
-      logo: Image.asset(
-        'image/logo.jpeg',fit: BoxFit.fill,),
-      navigator:null,
-      durationInSeconds: 7,
-      logoSize:250,
+    return EasySplashScreen(
+      backgroundColor: Colors.white24,
+    logo: Image.asset(
+    'image/logo.jpeg',fit: BoxFit.fill,),
+    navigator:null,
+    durationInSeconds: 7,
+    logoSize:250,
 
 
     );
