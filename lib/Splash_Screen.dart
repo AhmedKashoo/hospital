10d726dd62bb746599bce9ec79c36a2f7b7c6abf
hodpital <ahmed.kashoo99@gmail.com>
2022-03-13 12:12:38 +0200

    kaskoo

---
+++ lib/Splash_Screen.dart
@@ -0,0 +1,24 @@
+import 'package:easy_splash_screen/easy_splash_screen.dart';
+import 'package:flutter/cupertino.dart';
+
+class Splash_Screen extends StatefulWidget {
+  const Splash_Screen({Key? key}) : super(key: key);
+
+  @override
+  _Splash_ScreenState createState() => _Splash_ScreenState();
+}
+
+class _Splash_ScreenState extends State<Splash_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return  EasySplashScreen(
+      logo: Image.asset(
+        'images/logo.jpeg',fit: BoxFit.fill,),
+      navigator:null,
+      durationInSeconds: 7,
+      logoSize:250,
+
+
+    );
+  }
+}
